a84d1bd05957cbdd51f48fe795a608f5d85a7348
Débora Carvalho <debora.sesv@gmail.com>
2024-04-30 22:31:20 -0300

    Adds login validation: checks that the email is valid and that the password meets security criteria

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -1,10 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { H1 } from '../typography.ts/h1';
 import { Input } from '../input/input';
 import { Button } from '../button';
 import { Separator } from '../separator';
 
 export const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+
+  const isValidEmail = (email: string) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
+  const isValidPassword = (password: string) => {
+    const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{7,}$/;
+    return passwordRegex.test(password);
+  };
+
+  const validateFields = () => {
+    setEmailError('');
+    setPasswordError('');
+
+    if (!email.trim()) {
+      setEmailError('Campo obrigatório.');
+    } else if (!isValidEmail(email)) {
+      setEmailError('O email informado é inválido.');
+    }
+
+    if (!password.trim()) {
+      setPasswordError('Campo obrigatório.');
+    } else if (password.length < 7) {
+      setPasswordError('A senha deve ter pelo menos 7 caracteres.');
+    } else if (!isValidPassword(password)) {
+      setPasswordError('A senha deve ter pelo menos um dígito e uma letra.');
+    }
+  };
+  const handleSubmit = () => {
+    validateFields();
+    if (!emailError && !passwordError) {
+      console.log('Formulário enviado com sucesso!');
+    }
+  };
+
   const styleContainer = {
     display: 'grid',
     justifyContent: 'center',
@@ -15,13 +55,19 @@ export const Login = () => {
     <div style={styleContainer}>
       <H1>Bem-vindo(a) à TaqTile!</H1>
       <div>
-        <Input text="E-mail" />
+        <Input text="E-mail" value={email} onChange={(e) => setEmail(e.target.value)} error={emailError} />
         <Separator />
-        <Input text="Senha" password />
+        <Input
+          text="Senha"
+          password
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          error={passwordError}
+        />
       </div>
       <Separator />
       <div>
-        <Button>Entrar</Button>
+        <Button onClick={handleSubmit}>Entrar</Button>
       </div>
     </div>
   );
